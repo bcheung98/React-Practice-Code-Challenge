6acf33763570934669612ace519e34ef150423aa
Brian <briantcheung@gmail.com>
2021-03-23 14:07:10 -0700

    Completed assignment

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -3,15 +3,51 @@ import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
 
 // Endpoint!
-const API = "http://localhost:3000/sushis"
+const API = "http://localhost:3001/sushis"
 
 class App extends Component {
 
+  state = {
+    page: 1,
+    sushi: [],
+    plates: [],
+    money: 100
+  }
+
+  componentDidMount() {
+    this.getSushi();
+  }
+
+  getSushi = () => {
+    fetch(`${API}/?_limit=4&_page=${this.state.page}`)
+    .then(res => res.json())
+    .then(sushi => this.setState({sushi}));
+  }
+
+  nextPage = () => {
+    this.setState({
+      page: this.state.page + 1
+    }, this.getSushi());
+  }
+
+  addPlate = (price) => {
+    console.log(price);
+    if (this.state.money - price >= 0) {
+          this.setState({
+      plates: [
+        ...this.state.plates, "plate"
+      ],
+      money: this.state.money - price
+    });
+    }
+  }
+
   render() {
+    console.log(this.state.page, this.state.sushi);
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer sushiData={this.state.sushi} nextPage={this.nextPage} addPlate={this.addPlate} money={this.state.money} />
+        <Table plates={this.state.plates} money={this.state.money}/>
       </div>
     );
   }
--- sushi-saga-client/src/components/Sushi.js
+++ sushi-saga-client/src/components/Sushi.js
@@ -1,23 +1,53 @@
-import React, { Fragment } from 'react'
+import React, { Component } from 'react'
 
-const Sushi = (props) => {
-  return (
-    <div className="sushi">
-      <div className="plate" 
-           onClick={/* Give me a callback! */ null}>
-        { 
-          /* Tell me if this sushi has been eaten! */ 
-          false ?
-            null
-          :
-            <img src={/* Give me an image source! */ } width="100%" />
-        }
+class Sushi extends Component {
+
+  state = {
+    eaten: false
+  }
+
+  eatSushi = () => {
+    if (this.props.money > 0 && !this.state.eaten) {
+      this.setState({
+        eaten: true
+      }, () => this.props.addPlate(this.props.sushi.price))
+    }
+  };
+
+  render() {
+    let { name, img_url, price } = this.props.sushi
+    return (
+      <div className="sushi">
+        <div className="plate" onClick={this.eatSushi}>
+          {!this.state.eaten ? <img src={img_url} width="100%" /> : <img src={null} width="100%" />}
+        </div>
+        <h4 className="sushi-details">
+          {name} - ${price}
+        </h4>
       </div>
-      <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
-      </h4>
-    </div>
-  )
+    )
+  }
 }
-
 export default Sushi
+
+// const eatSushi = () => {
+//   console.log(":3");
+//   isEaten = true;
+// }
+
+// const Sushi = (props) => {
+//   let {name, img_url, price} = props.sushi
+//   return (
+//     <div className="sushi">
+//       <div className="plate" onClick={eatSushi}>
+//         { 
+//           /* Tell me if this sushi has been eaten! */ 
+//           !isEaten ? <img src={img_url} width="100%" /> : <img src={null} width="100%" />
+//         }
+//       </div>
+//       <h4 className="sushi-details">
+//         {name} - ${price}
+//       </h4>
+//     </div>
+//   )
+// }
